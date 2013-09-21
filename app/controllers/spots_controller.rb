1284143675823692390272ed0bd88b5fa34ad04e
Nat Welch <nat@natwelch.com>
2013-09-21 15:37:04 -0700

    view

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -3,6 +3,18 @@ class SpotsController < ApplicationController
   end
 
   def create
-    render text: params[:post].inspect
+    @spot = Spot.new(post_params)
+
+    @spot.save
+    redirect_to @spot
+  end
+
+  def show
+    @spot = Spot.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:spot).permit(:title, :text)
   end
 end
